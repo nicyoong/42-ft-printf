a3be705fc61bad6932f613c394a15fb95b623e7a
Nicholas Yoong <nicyoong321@gmail.com>
2024-11-08 20:50:51 +0800

    small changes

--- ft_printf.h
+++ ft_printf.h
@@ -4,14 +4,10 @@
 # include <stdarg.h>
 # include <unistd.h>
 # include <stdint.h>
+# include "libft.h"
 
-void	ft_putchar_fd(char c, int fd);
-void	ft_putstr_fd(char *str, int fd);
-void	ft_putnbr_fd(int n, int fd);
-void	ft_putnbr_unsigned_fd(unsigned int n, int fd);
 int		ft_putnbr_hex_fd(uintptr_t n, int fd, int uppercase);
-void	ft_putptr_fd(void *ptr, int fd);
-size_t	ft_strlen(const char *str);
+void	ft_putnbr_unsigned_fd(unsigned int n, int fd);
 int		num_len(int num);
 int		num_len_unsigned(unsigned int num);
 int		num_len_hex(unsigned int num);
@@ -22,7 +18,6 @@ int		handle_integer(va_list args);
 int		handle_unsigned(va_list args);
 int		handle_hex(va_list args, int uppercase);
 int		handle_percent();
-
 int		ft_printf(const char *format, ...);
 
 #endif
--- utils.c
+++ utils.c
@@ -1,3 +1,5 @@
+#include "ft_printf.h"
+
 int	num_len(int num)
 {
 	int	len;
@@ -46,4 +48,28 @@ int	num_len_hex(unsigned int num)
 		num /= 16;
 	}
 	return (len);
+}
+
+void	ft_putnbr_unsigned_fd(unsigned int n, int fd)
+{
+	if (n >= 10)
+		ft_putnbr_unsigned_fd(n / 10, fd);
+	ft_putchar_fd(n % 10 + '0', fd);
+}
+
+int	ft_putnbr_hex_fd(uintptr_t num, int fd, int is_upper)
+{
+	const char	*hex_digits;
+	int			count;
+
+	if (is_upper)
+		hex_digits = "0123456789ABCDEF";
+	else
+		hex_digits = "0123456789abcdef";
+	count = 0;
+	if (num >= 16)
+		count += ft_putnbr_hex_fd(num / 16, fd, is_upper);
+	ft_putchar_fd(hex_digits[num % 16], fd);
+	count++;
+	return (count);
 }
